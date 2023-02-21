4fd2e5778ad17f381f2149686bf868c5639b0450
Aeon-Domini <aeon.charfauros@knights.ucf.edu>
2023-02-21 18:27:55 -0500

    Movement
    1st Person movement + camera

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
-  public float moveSpeed;
+    public float moveSpeed;
 
     public Transform orientation;
 
@@ -15,31 +15,36 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
-    private void Start();
+    private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation =true;
+        rb =GetComponent<Rigidbody>();
+        rb.freezeRotation = true ;
     }
+
     private void Update()
     {
         MyInput();
-
     }
+
     private void FixedUpdate()
     {
         MovePlayer();
     }
 
+
+
     private void MyInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
     }
+
     private void MovePlayer()
-    {
-        // calculate movement direction
+   {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-    }
+   }
+
 }
