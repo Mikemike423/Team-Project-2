cbd0eae90b3b3c1450697ae6ad637bd04bb67bb6
fusionbolt18 <59746107+fusionbolt18@users.noreply.github.com>
2023-03-04 13:49:34 -0500

    Update AI_Enemy.cs

--- Assets/Scripts/AI_Enemy.cs
+++ Assets/Scripts/AI_Enemy.cs
@@ -41,7 +41,8 @@ public class AI_Enemy : MonoBehaviour
       //Set destination and 
       enemy.SetDestination(Player.position);
       transform.LookAt(Player);
-   }
+        transform.Rotate(0f, 90f, 0f, Space.Self);
+    }
 
    //Function to handle enenmy randomly searching while player is hiding
    private void Search() {
