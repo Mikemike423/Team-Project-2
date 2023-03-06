c09f94e920aef50c076192ab85a0e2e15af17677
Aeon-Domini <aeon.charfauros@knights.ucf.edu>
2023-03-05 20:57:19 -0500

    Revert "allBackup"
    This reverts commit 2f1bafadd0907ba9710deb5ab7053c198cc15576.

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -19,6 +19,7 @@ public class PauseController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       
         Resume();
         health = Enemy.GetComponent<HeathSystem>();
         CurrentLives();
